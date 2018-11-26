d844a47b15fbc8753ebb37db78dbc431d24229da
Derek <derek.baker89@gmail.com>
2018-11-26 14:55:20 +0000

    created form and stored items in empty array

--- todo_list.rb
+++ todo_list.rb
@@ -1,5 +1,13 @@
 require 'sinatra'
+require 'sinatra/reloader'
+
+list_of_items = []
+
+
 
 get '/' do
-    "Hello, World!"
+    item = params["item"]
+
+    list_of_items << item
+    erb :index, :locals => {:item => item, :list_of_items => list_of_items}
   end
